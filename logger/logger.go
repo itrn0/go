ac362e61f0b2d93f118df8f8fd23c15eedf43606
itrn0 <ilshat.mgn@gmail.com>
2019-06-03 15:31:10 +0500

    Init

---
+++ logger/logger.go
@@ -0,0 +1,77 @@
+package logger
+
+var (
+	defaultLogger Logger
+)
+
+func GetLogger() Logger {
+	return defaultLogger
+}
+
+func SetLogger(l Logger) {
+	defaultLogger = l
+}
+
+func Debugf(format string, args ...interface{}) {
+	defaultLogger.Debugf(format, args...)
+}
+
+func Infof(format string, args ...interface{}) {
+	defaultLogger.Infof(format, args...)
+}
+
+func Warnf(format string, args ...interface{}) {
+	defaultLogger.Warnf(format, args...)
+}
+
+func Errorf(format string, args ...interface{}) {
+	defaultLogger.Errorf(format, args...)
+}
+
+func Fatalf(format string, args ...interface{}) {
+	defaultLogger.Fatalf(format, args...)
+}
+
+func Panicf(format string, args ...interface{}) {
+	defaultLogger.Panicf(format, args...)
+}
+
+func Debug(args ...interface{}) {
+	defaultLogger.Debug(args...)
+}
+
+func Info(args ...interface{}) {
+	defaultLogger.Info(args...)
+}
+
+func Warn(args ...interface{}) {
+	defaultLogger.Warn(args...)
+}
+
+func Error(args ...interface{}) {
+	defaultLogger.Error(args...)
+}
+
+func Fatal(args ...interface{}) {
+	defaultLogger.Fatal(args...)
+}
+
+func Panic(args ...interface{}) {
+	defaultLogger.Panic(args...)
+}
+
+type Logger interface {
+	Debugf(format string, args ...interface{})
+	Infof(format string, args ...interface{})
+	Warnf(format string, args ...interface{})
+	Errorf(format string, args ...interface{})
+	Fatalf(format string, args ...interface{})
+	Panicf(format string, args ...interface{})
+
+	Debug(args ...interface{})
+	Info(args ...interface{})
+	Warn(args ...interface{})
+	Error(args ...interface{})
+	Fatal(args ...interface{})
+	Panic(args ...interface{})
+}
